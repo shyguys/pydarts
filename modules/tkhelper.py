757657b93d9625b2d52b51dfbb2ded1e33d5765c
Neufeld-Martin <m.neufeld946@gmail.com>
2023-01-08 16:39:27 +0100

    feat: walk_children() max_depth

--- modules/tkhelper.py
+++ modules/tkhelper.py
@@ -1,13 +1,19 @@
 import tkinter as tk
 from typing import Generator
 
-def walk_children(widget: tk.Widget) -> Generator[tk.Widget, None, None]:
-    children = widget.winfo_children()
+def walk_children(
+        widget: tk.Widget, max_depth: int = -1
+        ) -> Generator[tk.Widget, None, None]:
+    children = [
+        (child, max_depth) for child in reversed(widget.winfo_children())
+    ]
     while children:
-        child = children.pop()
+        child, max_depth = children.pop()
         yield child
+        if max_depth == 0:
+            continue
         for grandchild in reversed(child.winfo_children()):
-            children.append(grandchild)
+            children.append((grandchild, max_depth-1))
 
 def is_parent_of(parent: tk.Widget, widget: tk.Widget) -> bool:
     for child in walk_children(parent):

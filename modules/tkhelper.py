a72a4e00529aeb15ddbfeea86c440dbfa2c0c7b5
Neufeld-Martin <m.neufeld946@gmail.com>
2023-01-08 13:31:57 +0100

    feat: tkhelper lib

---
+++ modules/tkhelper.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from typing import Generator
+
+def walk_children(widget: tk.Widget) -> Generator[tk.Widget, None, None]:
+    children: list[tk.Widget] = widget.winfo_children()
+    while children:
+        child: tk.Widget = children.pop()
+        yield child
+        for grandchild in reversed(child.winfo_children()):
+            children.append(grandchild)
+
+def is_parent_of(parent: tk.Widget, widget: tk.Widget) -> bool:
+    for child in walk_children(parent):
+        if child is widget:
+            return True
+    return False

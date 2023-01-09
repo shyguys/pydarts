5be53fafbd8f3d49334cd5a8c8045fd8746a7405
Neufeld-Martin <m.neufeld946@gmail.com>
2023-01-09 22:59:21 +0100

    feat: bind_children

--- modules/tkhelper.py
+++ modules/tkhelper.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from typing import Generator
 
+
 def walk_children(widget: tk.Widget, max_depth: int = -1) -> Generator[tk.Widget, None, None]:
     children = [(child, max_depth) for child in reversed(widget.winfo_children())]
     while children:
@@ -10,6 +11,11 @@ def walk_children(widget: tk.Widget, max_depth: int = -1) -> Generator[tk.Widget
             continue
         for grandchild in reversed(child.winfo_children()):
             children.append((grandchild, max_depth-1))
+
+# [TODO]: add type hint to 'func' if applicable
+def bind_children(widget: tk.Widget, sequence: str, func):
+    for child in walk_children(widget):
+        child.bind(sequence, func)
 
 def is_parent_of(parent: tk.Widget, widget: tk.Widget) -> bool:
     for child in walk_children(parent):

3b5c82c8fe5d745cf13706b8306f1f113bf3cefd
Neufeld-Martin <m.neufeld946@gmail.com>
2023-06-04 09:36:38 +0200

    refactor: format ListVar

--- pydarts/help/tkh.py
+++ pydarts/help/tkh.py
@@ -1,34 +1,41 @@
 import tkinter as tk
-from typing import Generator
+from typing import Callable, Generator
 
 
 class ListVar():
     """Mimics a tk.Variable for a list."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._list_var = tk.StringVar()
         self._list = []
+        return None
 
     def get(self) -> list[str]:
         """Returns a copy of the variable."""
         return self._list.copy()
 
-    def set(self, value: list[str]):
+    def set(self, value: list[str]) -> None:
         """Sets the variable to a value."""
         self._list = value
-        self._list_var.set(value)
+        return self._list_var.set(value)
 
-    def trace_add(self, *args, **kwargs):
+    def trace_add(self, mode: str, callback: Callable) -> str:
         """As defined in tk.Variable.trace_add()."""
-        self._list_var.trace_add(*args, **kwargs)
+        valid_modes = ["write"]
+        if mode not in valid_modes:
+            raise ValueError(
+                f"Trace mode {mode!r} not supported. "
+                f"Must be one of {valid_modes!r}."
+            )
+        return self._list_var.trace_add(mode, callback)
 
-    def trace_info(self):
+    def trace_info(self) -> list[tuple[tuple[str, ...], str]]:
         """As defined in tk.Variable.trace_info()."""
-        self._list_var.trace_info()
+        return self._list_var.trace_info()
 
-    def trace_remove(self, *args, **kwargs):
+    def trace_remove(self, mode: str, cbname: str) -> None:
         """As defined in tk.Variable.trace_remove()."""
-        self._list_var.trace_remove(*args, **kwargs)
+        return self._list_var.trace_remove(mode, cbname)
 
 
 def walk_children(
@@ -36,13 +43,11 @@ def walk_children(
     max_depth: int = -1
 ) -> Generator[tk.Misc, None, None]:
     """Returns a widget's children as generator."""
-    children = [
-        (child, max_depth) for child in reversed(widget.winfo_children())
-    ]
+    children = [(child, max_depth) for child in reversed(widget.winfo_children())]
     while children:
         child, max_depth = children.pop()
         yield child
         if max_depth == 0:
             continue
         for grandchild in reversed(child.winfo_children()):
-            children.append((grandchild, max_depth-1))
+            children.append((grandchild, max_depth - 1))
